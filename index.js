100d36a7cea04e190860f5705b4751c88bd05d4f
kaagyebi <kaagyebi@gmail.com>
2025-06-15 15:30:56 -0700

    added cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 import { weatherRouter } from "./routes/weather.route.js";
+import cors from "cors"
 
 const app = express();
 
 const PORT = process.env.PORT || 6161;
 
 app.use(express.json());
+app.use(cors())
 app.use("/api/v1", weatherRouter)
 
 const mongo_URI = process.env.MONGO_URI;
